65c4abb50ab784885928e04a9d67417232251b68
Joseph Urso <joeurso@josephs-MBP.com>
2022-01-23 14:51:52 -0800

    Create skeleton set up of BattleInfo and RapperStats component

---
+++ client/src/components/BattleInfo/BattleInfo.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BattleInfo() {
+    return <div></div>;
+}
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,41 @@
 import React, { Component } from "react";
+import BattleInfo from "../components/BattleInfo/BattleInfo";
+import RapperStats from "../components/RapperStats/RapperStats";
 import "./Homepage.scss";
 
 export default class HomePage extends Component {
+    state = {
+        rappers: [],
+        battles: [],
+    };
+
     render() {
         return (
             <div>
                 <h1>WHOSE DA ILLEST?</h1>
-                <h2>YEAH YEAH</h2>
-                <p>rap rap rap rap rap rap rap rap rap</p>
+                <button>Go To Battles</button>
+                <section>
+                    <h2>On Da Mic</h2>
+                    <div>
+                        <h3>NAME</h3>
+                        <h3>W-L</h3>
+                        <h3>AVG GRADE</h3>
+                    </div>
+                    {this.state.rappers.map((rapper) => {
+                        <RapperStats rapper={rapper} avgGrade={10} />;
+                    })}
+                </section>
+                <section>
+                    <h2>Battle Board</h2>
+                    <div>
+                        <h3>NAME</h3>
+                        <h3>RAPPERS</h3>
+                        <h3>BATTLES FOUGHT</h3>
+                    </div>
+                    {this.state.battles.map((battle) => {
+                        <BattleInfo battle={battle} />;
+                    })}
+                </section>
             </div>
         );
     }
