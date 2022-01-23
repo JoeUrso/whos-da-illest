72a95e98811a3d99090a48fa6eebd32c84cc3a81
Joseph Urso <joeurso@josephs-MBP.com>
2022-01-23 13:42:00 -0800

    Set up all partials for styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,34 @@
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import "./App.scss";
-// TODO IMPORT COMPONENTS HERE
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.scss";
+import HomePage from "./pages/HomePage";
 
-// function App() {
-//     return (
-//         <Router>
-//             <Header />
-//             <div className="switch__container">
-//                 <Switch>
-//                     <Route
-//                         exact
-//                         path="/"
-//                         component={HomePage} // homepage
-//                     />
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route
+                    exact
+                    path="/"
+                    component={HomePage} // homepage
+                />
 
-//                     <Route
-//                         exact
-//                         path="/battles/:id"
-//                         component={BattlePage} // battle page
-//                     />
+                {/* <Route
+                        exact
+                        path="/battles/:id"
+                        component={BattlePage} // battle page
+                    />
 
-//                     <Route
-//                         path="/battles/:id/:rapperId"
-//                         component={ArtistGrade} //artist grade
-//                     />
+                    <Route
+                        path="/battles/:id/:rapperId"
+                        component={ArtistGrade} //artist grade
+                    />
 
-//                     <Route
-//                         path="/battles/:id/results"
-//                         component={BattleResults} //battle results
-//                     />
-//                 </Switch>
-//                 <Footer />
-//             </div>
-//         </Router>
-//     );
-// }
-// export default App;
+                    <Route
+                        path="/battles/:id/results"
+                        component={BattleResults} //battle results
+                    /> */}
+            </Switch>
+        </Router>
+    );
+}
+export default App;
