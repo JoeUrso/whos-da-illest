52b0b98e59bb901dbfaf9d4aefbedb87fa6ff832
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-01-23 13:42:00 -0800

    Set up all partials for styling

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Homepage.scss";
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>WHOSE DA ILLEST?</h1>
+                <h2>YEAH YEAH</h2>
+                <p>rap rap rap rap rap rap rap rap rap</p>
+            </div>
+        );
+    }
+}
