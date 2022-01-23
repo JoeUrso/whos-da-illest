30c4984b2940caa5dcc66a313cde7344168d1712
Joseph Urso <joeurso@josephs-MBP.com>
2022-01-22 17:59:48 -0800

    Add seed data to all tables in db

---
+++ server/seed_data/grades.js
@@ -0,0 +1,302 @@
+module.exports = [
+    {
+        id: 1,
+        grade: 88,
+        rapper_id: 1,
+    },
+    {
+        id: 2,
+        grade: 89,
+        rapper_id: 1,
+    },
+    {
+        id: 3,
+        grade: 96,
+        rapper_id: 1,
+    },
+    {
+        id: 4,
+        grade: 88,
+        rapper_id: 2,
+    },
+    {
+        id: 5,
+        grade: 90,
+        rapper_id: 2,
+    },
+    {
+        id: 6,
+        grade: 91,
+        rapper_id: 2,
+    },
+    {
+        id: 7,
+        grade: 88,
+        rapper_id: 3,
+    },
+    {
+        id: 8,
+        grade: 99,
+        rapper_id: 3,
+    },
+    {
+        id: 9,
+        grade: 87,
+        rapper_id: 3,
+    },
+    {
+        id: 10,
+        grade: 92,
+        rapper_id: 4,
+    },
+    {
+        id: 11,
+        grade: 88,
+        rapper_id: 4,
+    },
+    {
+        id: 12,
+        grade: 87,
+        rapper_id: 4,
+    },
+    {
+        id: 13,
+        grade: 98,
+        rapper_id: 5,
+    },
+    {
+        id: 14,
+        grade: 90,
+        rapper_id: 5,
+    },
+    {
+        id: 15,
+        grade: 88,
+        rapper_id: 5,
+    },
+    {
+        id: 16,
+        grade: 88,
+        rapper_id: 6,
+    },
+    {
+        id: 17,
+        grade: 84,
+        rapper_id: 6,
+    },
+    {
+        id: 18,
+        grade: 90,
+        rapper_id: 6,
+    },
+    {
+        id: 19,
+        grade: 88,
+        rapper_id: 7,
+    },
+    {
+        id: 20,
+        grade: 93,
+        rapper_id: 7,
+    },
+    {
+        id: 21,
+        grade: 89,
+        rapper_id: 7,
+    },
+    {
+        id: 22,
+        grade: 88,
+        rapper_id: 8,
+    },
+    {
+        id: 23,
+        grade: 87,
+        rapper_id: 8,
+    },
+    {
+        id: 24,
+        grade: 83,
+        rapper_id: 8,
+    },
+    {
+        id: 25,
+        grade: 88,
+        rapper_id: 9,
+    },
+    {
+        id: 26,
+        grade: 92,
+        rapper_id: 9,
+    },
+    {
+        id: 27,
+        grade: 86,
+        rapper_id: 9,
+    },
+    {
+        id: 28,
+        grade: 90,
+        rapper_id: 10,
+    },
+    {
+        id: 29,
+        grade: 88,
+        rapper_id: 10,
+    },
+    {
+        id: 30,
+        grade: 94,
+        rapper_id: 10,
+    },
+    {
+        id: 31,
+        grade: 90,
+        rapper_id: 11,
+    },
+    {
+        id: 32,
+        grade: 95,
+        rapper_id: 11,
+    },
+    {
+        id: 33,
+        grade: 94,
+        rapper_id: 11,
+    },
+    {
+        id: 34,
+        grade: 88,
+        rapper_id: 12,
+    },
+    {
+        id: 35,
+        grade: 89,
+        rapper_id: 12,
+    },
+    {
+        id: 36,
+        grade: 93,
+        rapper_id: 12,
+    },
+    {
+        id: 37,
+        grade: 88,
+        rapper_id: 13,
+    },
+    {
+        id: 38,
+        grade: 94,
+        rapper_id: 13,
+    },
+    {
+        id: 39,
+        grade: 91,
+        rapper_id: 13,
+    },
+    {
+        id: 40,
+        grade: 88,
+        rapper_id: 14,
+    },
+    {
+        id: 41,
+        grade: 92,
+        rapper_id: 14,
+    },
+    {
+        id: 42,
+        grade: 90,
+        rapper_id: 14,
+    },
+    {
+        id: 43,
+        grade: 88,
+        rapper_id: 15,
+    },
+    {
+        id: 44,
+        grade: 89,
+        rapper_id: 15,
+    },
+    {
+        id: 45,
+        grade: 85,
+        rapper_id: 15,
+    },
+    {
+        id: 46,
+        grade: 88,
+        rapper_id: 16,
+    },
+    {
+        id: 47,
+        grade: 90,
+        rapper_id: 16,
+    },
+    {
+        id: 48,
+        grade: 91,
+        rapper_id: 16,
+    },
+    {
+        id: 49,
+        grade: 88,
+        rapper_id: 17,
+    },
+    {
+        id: 50,
+        grade: 89,
+        rapper_id: 17,
+    },
+    {
+        id: 51,
+        grade: 82,
+        rapper_id: 17,
+    },
+    {
+        id: 52,
+        grade: 88,
+        rapper_id: 18,
+    },
+    {
+        id: 53,
+        grade: 89,
+        rapper_id: 18,
+    },
+    {
+        id: 54,
+        grade: 92,
+        rapper_id: 18,
+    },
+    {
+        id: 55,
+        grade: 88,
+        rapper_id: 19,
+    },
+    {
+        id: 56,
+        grade: 93,
+        rapper_id: 19,
+    },
+    {
+        id: 57,
+        grade: 87,
+        rapper_id: 19,
+    },
+    {
+        id: 58,
+        grade: 88,
+        rapper_id: 20,
+    },
+    {
+        id: 59,
+        grade: 89,
+        rapper_id: 20,
+    },
+    {
+        id: 60,
+        grade: 90,
+        rapper_id: 20,
+    },
+];
