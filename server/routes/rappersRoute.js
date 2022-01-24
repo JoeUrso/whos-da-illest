26831c114c4b5de51ac01e9c4d409722c4888620
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-01-23 20:19:14 -0800

    GET battles from backend

--- server/routes/rappersRoute.js
+++ server/routes/rappersRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const rappersController = require("../controllers/rappersController");
 
 router.route("/").get(rappersController.index);
+router.route("/with-grades").get(rappersController.rappersWithGrades);
 
 module.exports = router;
