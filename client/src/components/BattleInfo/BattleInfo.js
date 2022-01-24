1bfd374e8bc95220323b00e81941fa5047705ddf
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-01-23 17:05:07 -0800

    Style BattleInfo component

--- client/src/components/BattleInfo/BattleInfo.js
+++ client/src/components/BattleInfo/BattleInfo.js
@@ -1,5 +1,29 @@
 import React from "react";
+import "./BattleInfo.scss";
 
-export default function BattleInfo() {
-    return <div></div>;
+export default function BattleInfo({ battle }) {
+    return (
+        <article className="battle-info">
+            <div className="battle-info__name-container">
+                <h3 className="battle-info__card-headings">BATTLE NAME</h3>
+                <p className="battle-info__name">{battle.name}</p>
+            </div>
+            <div className="battle-info__rappers-battles-fought-container">
+                <div className="battle-info__rappers-container">
+                    <h3 className="battle-info__card-headings">RAPPERS</h3>
+                    <p className="battle-info__rappers">
+                        {battle.rapper1_id} vs {battle.rapper2_id}
+                    </p>
+                </div>
+                <div className="battle-info__battles-fought-container">
+                    <h3 className="battle-info__card-headings">
+                        BATTLES FOUGHT
+                    </h3>
+                    <p className="battle-info__total-battles">
+                        {battle.total_battles}
+                    </p>
+                </div>
+            </div>
+        </article>
+    );
 }
