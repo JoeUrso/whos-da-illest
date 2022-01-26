08ecc89c77386ade241fc10a006e5aa99fd6be40
Joseph Urso <joeurso@josephs-MBP.com>
2022-01-25 16:17:56 -0800

    Create skeleton of BattlePage and add route to app

---
+++ client/src/pages/BattlePage/BattlePage.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import React, { Component } from "react";
+const client_id = "";
+const client_secret = "";
+const redirect_url = "localhost:3000/battles/:id";
+
+export default class BattlePage extends Component {
+    state = {
+        rapper1: [],
+        rapper2: [],
+    };
+
+    componentDidMount = () => {
+        axios.get;
+    };
+
+    render() {
+        return <div></div>;
+    }
+}
