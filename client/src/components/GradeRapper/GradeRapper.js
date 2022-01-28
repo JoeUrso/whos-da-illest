8b05ea621e5ba28bd7fb37c4a8dfb5f2d93efb67
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-01-27 18:24:31 -0800

    Create an array of  criteria for grading and map the array

--- client/src/components/GradeRapper/GradeRapper.js
+++ client/src/components/GradeRapper/GradeRapper.js
@@ -2,9 +2,40 @@ import React from "react";
 import "./GradeRapper.scss";
 
 export default function GradeRapper({ rapper, buttonText, click }) {
+    const criteria = [
+        "Mic Presence",
+        "Flow",
+        "Rhymes",
+        "Vocabulary",
+        "Articulation",
+        "Creativity",
+        "Versatility",
+        "Voice",
+        "Hits",
+        "Performance",
+    ];
+
     return (
         <section className="grade">
             <h2 className="grade__rapper-name">{rapper.name}</h2>
+            <article className="grade__container">
+                {criteria.map((criterion) => {
+                    return (
+                        <>
+                            <h3>{criterion}</h3>
+                            <div className="grade__slider-container">
+                                <input
+                                    type="range"
+                                    min="0"
+                                    max="10"
+                                    id="grade"
+                                    className="grade__slider"
+                                ></input>
+                            </div>
+                        </>
+                    );
+                })}
+            </article>
             <button className="grade__button" onClick={click}>
                 {buttonText}
             </button>
